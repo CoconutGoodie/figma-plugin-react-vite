a397dd61866e5497968b97cfaa9948016f263e14
Anılcan Metinyurt <igoodie@programmer.net>
2023-03-29 18:33:52 +0300

    fix: move ui.html to index.html for dev purposes

--- vite.config.plugin.ts
+++ vite.config.plugin.ts
@@ -8,7 +8,7 @@ const figmaManifest = {
   id: "1222852692367737510",
   api: "1.0.0",
   main: "plugin.js",
-  ui: "ui.html",
+  ui: "index.html",
   capabilities: [],
   enableProposedApi: false,
   editorType: ["figma", "figjam"],
--- vite.config.ui.ts
+++ vite.config.ui.ts
@@ -8,12 +8,16 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), inlineSvg(), svgr(), viteSingleFile()],
+  server: {
+    base: "/ui.html",
+    port: 4000,
+  },
   root: path.resolve(__dirname, "./src/ui/"),
   build: {
     outDir: path.resolve(__dirname, "./dist"),
     rollupOptions: {
       input: {
-        ui: path.relative(__dirname, "./src/ui/ui.html"),
+        ui: path.relative(__dirname, "./src/ui/index.html"),
       },
       output: {
         entryFileNames: "[name].js",
