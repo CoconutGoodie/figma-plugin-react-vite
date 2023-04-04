5ade4dc4e541494b47a22fa0c18bec6c82d68880
Anılcan Metinyurt <igoodie@programmer.net>
2023-04-04 07:30:43 +0300

    feat: update monorepo-networker

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -3,17 +3,22 @@ import { NetworkMessages } from "@common/network/messages";
 import { NetworkSide } from "@common/network/sides";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app";
 
-initializeNetwork(NetworkSide.UI);
+async function bootstrap() {
+  initializeNetwork(NetworkSide.UI);
 
-NetworkMessages.HELLO_PLUGIN.send({ text: "Hey there, Figma!" });
+  NetworkMessages.HELLO_PLUGIN.send({ text: "Hey there, Figma!" });
 
-const rootElement = document.getElementById("root") as HTMLElement;
-const root = ReactDOM.createRoot(rootElement);
+  const App = (await import("./app")).default;
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  const rootElement = document.getElementById("root") as HTMLElement;
+  const root = ReactDOM.createRoot(rootElement);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+bootstrap();
